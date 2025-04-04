f3bf34fae54edf79b5574f99eee1078f402fe639
Ultorem <108500261+Ultorem@users.noreply.github.com>
2025-04-04 09:48:13 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,13 @@
 import { useConfig } from '@/lib/config'
 import Vercel from '@/components/Vercel'
+
 const Footer = ({ fullWidth }) => {
   const BLOG = useConfig()
 
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
+
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -18,7 +20,15 @@ const Footer = ({ fullWidth }) => {
           <p>
             © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`} |{' '}
             <a href="https://100kryssord.com" target="_blank" rel="noopener noreferrer">
-              Kryssordhjelp
+              Kryssordhjelp og kryssordløsninger
+            </a>{' '}
+            |{' '}
+            <a href="https://kryssordhjelp.com" target="_blank" rel="noopener noreferrer">
+              Finn hjelp til kryssord på kryssordhjelp.com
+            </a>{' '}
+            |{' '}
+            <a href="https://www.tekstmaler.no/" target="_blank" rel="noopener noreferrer">
+              Tekstforslag, vitser, gåter og maler – tekstmaler.no
             </a>
           </p>
           <Vercel />
